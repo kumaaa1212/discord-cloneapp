cbe7e764c3fee501c771d4ac9bb13eb98d4d4d0b
Ryohei <harrry.kuma1225@gmail.com>
2023-06-02 16:22:33 +0900

    commit

--- firebase.ts
+++ firebase.ts
@@ -2,14 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { GoogleAuthProvider, getAuth } from "firebase/auth";
 const firebaseConfig = {
-  apiKey: "AIzaSyAnrGMV9sDCgOGAQO5YRaQDEjAo6pPQjFc",
-  authDomain: "discord-clone-7e751.firebaseapp.com",
-  projectId: "discord-clone-7e751",
-  storageBucket: "discord-clone-7e751.appspot.com",
-  messagingSenderId: "133435442635",
-  appId: "1:133435442635:web:89547bad87d84e462b224e"
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID
 };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
